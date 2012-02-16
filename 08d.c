224cf866bafecff939128ecc47b1c76ce08a996e
7shi <7shi@live.jp>
2012-02-17 07:29:16 +0900

    サンク作成を関数に分割

--- 08d.c
+++ 08d.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <windows.h>
 
+void create_thunk(unsigned char *thunk, void *this, void *func) {
+    // push this
+    thunk[0] = 0x68;
+    *(void **)&thunk[1] = this;
+    // mov eax, show
+    thunk[5] = 0xb8;
+    *(void **)&thunk[6] = func;
+    // call eax
+    thunk[10] = 0xff;
+    thunk[11] = 0xd0;
+    // add esp, 4
+    thunk[12] = 0x83;
+    thunk[13] = 0xc4;
+    thunk[14] = 4;
+    // ret
+    thunk[15] = 0xc3;
+}
+
 typedef struct tagNumber Number;
 struct tagNumber {
     void (*show)(void);
@@ -19,21 +37,7 @@ void Number_init(Number *this, void (*show)(), int n) {
     thunk = (unsigned char *)
         VirtualAlloc(NULL, 16, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     this->show = (void (*)(void))thunk;
-    // push this
-    thunk[0] = 0x68;
-    *(void **)&thunk[1] = this;
-    // mov eax, show
-    thunk[5] = 0xb8;
-    *(void **)&thunk[6] = show;
-    // call eax
-    thunk[10] = 0xff;
-    thunk[11] = 0xd0;
-    // add esp, 4
-    thunk[12] = 0x83;
-    thunk[13] = 0xc4;
-    thunk[14] = 4;
-    // ret
-    thunk[15] = 0xc3;
+    create_thunk(thunk, this, show);
 }
 
 Number *Number_new(int n) {
