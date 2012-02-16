c7331819f5440ffaa202fad9436e38d99d493a63
7shi <7shi@live.jp>
2012-02-17 01:18:23 +0900

    ヒープを前提に簡略化

---
+++ 05c.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int a, b;
+} Pair;
+
+Pair *Pair_new(int a, int b) {
+    Pair *this = (Pair *)malloc(sizeof(Pair));
+    this->a = a;
+    this->b = b;
+    return this;
+}
+
+void Pair_delete(Pair *this) {
+    printf("%d, %d\n", this->a, this->b);
+    free(this);
+}
+
+int Pair_add(Pair *this) {
+    return this->a + this->b;
+}
+
+int main(){
+    Pair *pair = Pair_new(1, 2);
+    printf("%d\n", Pair_add(pair));
+    Pair_delete(pair);
+    return 0;
+}
